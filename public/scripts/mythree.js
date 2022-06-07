5f3c3bc53a7b4d0c72e7fe61ba4d5c487065bb01
Julien Gelles <julien.gelles.etu@univ-lille.fr>
2022-06-07 15:40:47 +0200

    merge

--- public/scripts/mythree.js
+++ public/scripts/mythree.js
@@ -37,23 +37,62 @@ export function main(){
   // ajout des meshs
   let geometry = new THREE.PlaneGeometry( 1, ysizehtml/xsizehtml );
   let texture = new THREE.TextureLoader().load( imageSrc );
-  let material = new THREE.MeshBasicMaterial( { map: texture , side: THREE.DoubleSide} );
-  let plane = new THREE.Mesh( geometry, material );
+  let materialplan = new THREE.MeshBasicMaterial( { map: texture , side: THREE.DoubleSide} );
+  let plane = new THREE.Mesh( geometry, materialplan );
   scene.add( plane );
+
+  let ballGeo = new THREE.BoxGeometry( 0.01, 0.01, 0.01 );
+  let ballMat = new THREE.MeshBasicMaterial( {color: "rgb(125,125,125)"} );
+  let ball = new THREE.Mesh(ballGeo, ballMat);
+  ball.position.z = 0.5;
 
   for (let i=-4; i<5; i++) {
     for (let j=-4 ; j<5; j++) {
 
-      let geometry2 = new THREE.BoxGeometry( 0.01, 0.01, 0.01 );
-      let material2 = new THREE.MeshBasicMaterial( {color: "rgb("+ (i+4)*30 +", "+ (j+4)*30 +", "+ (0+4)*30 +")"} );
-      let cube = new THREE.Mesh( geometry2, material2 );
+      let cubeGeo = new THREE.BoxGeometry( 0.01, 0.01, 0.01 );
+      ball.updateMatrix();
+      cubeGeo.merge(ball.geometry, ball.matrix);
+
+      let cubeMat = new THREE.MeshBasicMaterial( {color: "rgb("+ (i+4)*30 +", "+ (j+4)*30 +", "+ (0+4)*30 +")"} );
+      let cube = new THREE.Mesh( cubeGeo, cubeMat );
       cube.position.x = i* 0.1;
       cube.position.y = j* 0.1;
       cube.position.z = 0.15;
-      scene.add( cube );
+      scene.add(cube);
 
     }    
   }
+
+  // function generateCube(nCubes, generated){
+  //   let geometry 	= new THREE.CubeGeometry( 50, 50, 50 );
+  //   let material 	= new THREE.MeshNormalMaterial();
+  //   let mesh	= new THREE.Mesh( geometry, material );
+  
+  //   for ( let generated = 0; nCubes > generated; generated++ ) {
+  //     mesh.position.x = Math.random() * 300 - 150;
+  //     mesh.position.y = Math.random() * 300 - 150;
+  //     mesh.position.z = Math.random() * 300 - 150;
+  //     mesh.rotation.x = Math.random() * 360 * ( Math.PI / 180 );
+  //     mesh.rotation.y = Math.random() * 360 * ( Math.PI / 180 );
+  //     THREE.GeometryUtils.merge(mergedGeo, mesh);
+  //   }
+    
+  //   if( nCubes !== generated){
+  //     setTimeout(function(){	generateCube(nCubes, generated);	    }, 0)
+  //   }
+  
+  //   if( nCubes === generated ){
+  //     mergedGeo.computeFaceNormals();
+  //     group	= new THREE.Mesh( mergedGeo, material );
+  //     group.updateMatrix();
+  //     scene.add( group );					
+  //   }
+    
+  // }
+
+  // let group 		= new THREE.Object3D();					
+  // let mergedGeo	= new THREE.Geometry();
+  // generateCube(100, 0);
 
   
   // fonction du rendu en boucle
