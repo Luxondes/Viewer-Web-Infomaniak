0d9e2065cbef24fda4268543e045ff45d8427034
Julien Gelles <julien.gelles.etu@univ-lille.fr>
2022-06-15 01:04:46 +0200

    1er essai heightmap choisie

--- public/scripts/heightMap.js
+++ public/scripts/heightMap.js
@@ -43,24 +43,43 @@ let heatVertex = `
   let heightMap = new THREE.CanvasTexture(canvasH);
   let ctx = canvasH.getContext("2d");
   
-  //console.log(heightMap);
+  let dataM = [
+    [0, 0, 240], [0, 50, 120], [0, 100, 148], [0, 150, 8], [0, 200, 74],
+    [50, 0, 9], [50, 50, 145], [50, 100, 124], [50, 150, 74], [50, 200, 95],
+    [100, 0, 56], [100, 50, 115], [100, 100, 74], [100, 150, 234], [100, 200, 7],
+    [150, 0, 86], [150, 50, 227], [150, 100, 146], [150, 150, 92], [150, 200, 143],
+    [200, 0, 178], [200, 50, 11], [200, 100, 247], [200, 150, 89], [200, 200, 160]
+  ]
+
+
+
   createHeightMap();
   function createHeightMap() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 256, 256);
-    for (let i = 0; i < 100; i++) {
-      let x = Math.floor(Math.random() * 255);
-      let y = Math.floor(Math.random() * 255);
-      let radius = 50;
-      let grd = ctx.createRadialGradient(x, y, 1, x, y, radius);
-      let h8 = Math.floor(Math.random() * 255);
-      grd.addColorStop(0, "rgb(" + h8 + "," + h8 + "," + h8 + ")");
+    // for (let i = 0; i < 1; i++) {
+    //   let x = Math.floor(Math.random() * 255);
+    //   let y = Math.floor(Math.random() * 255);
+    //   let radius = 50;
+    //   let grd = ctx.createRadialGradient(x, y, 1, x, y, radius);
+    //   let h8 = Math.floor(Math.random() * 255);
+    //   grd.addColorStop(0, "rgb(" + h8 + "," + h8 + "," + h8 + ")");
+    //   grd.addColorStop(1, "transparent");
+    //   ctx.fillStyle = grd;
+    //   ctx.fillRect(0, 0, 256, 256);
+    // }
+    for (let index = 0; index < dataM.length; index++) {
+      let grd = ctx.createRadialGradient(dataM[index][0]+25, dataM[index][1]+25, 1, dataM[index][0]+25, dataM[index][1]+25, 100);
+      let grey = dataM[index][2]
+      grd.addColorStop(0, "rgb(" + grey + "," + grey + "," + grey + ")");
       grd.addColorStop(1, "transparent");
       ctx.fillStyle = grd;
       ctx.fillRect(0, 0, 256, 256);
     }
     heightMap.needsUpdate = true;
   }
+
+  console.log(dataM);
 
 
 
@@ -133,14 +152,21 @@ plane.rotation.x = -Math.PI / 2;
 plane.position.y = 1;
 scene.add( plane );
 
-let planeGeometry = new THREE.PlaneBufferGeometry(50, 50, 500, 500);
+const data = {
+  width: 50,
+  height: 50,
+  widthSegments: 500,
+  heightSegments: 500,
+};
+
+let planeGeometry = new THREE.PlaneBufferGeometry(data.width, data.height, data.widthSegments, data.heightSegments);
 planeGeometry.rotateX(-Math.PI * 0.5);
 
 let heat = new THREE.Points(planeGeometry, new THREE.ShaderMaterial({
   uniforms: {
     heightMap: {value: heightMap},
     heightRatio: {value: 10},
-    heightBias: {value: 10},
+    heightBias: {value: 7},
     gradientMap: {value: gradientMap}
   },
   vertexShader: heatVertex,
@@ -148,6 +174,10 @@ let heat = new THREE.Points(planeGeometry, new THREE.ShaderMaterial({
 }));
 
 scene.add(heat);
+
+  var gui = new dat.GUI();
+  gui.add(heat.material.uniforms.heightRatio, "value", 0, 25).name("heightRatio");
+  gui.add(heat.material.uniforms.heightBias, "value", 1, 10).name("heightBias");
 
 window.addEventListener( 'resize', onWindowResize, false );
 
